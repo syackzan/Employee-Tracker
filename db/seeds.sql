aad82d5f263a6169a5b93c7fed1e670c86888440
Scotty Yackzan <syackzan@gmail.com>
2021-09-16 20:02:56 -0700

    Added & Updated Seed file with more information to roles and employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,19 +5,26 @@ VALUES ("Executive Department"),
        ("Sales Department"),
        ("Project Management Department");
 
-VALUES ("CEO", 150000, 1),
-	   ("Executive Assistant", 60000, 1),
-       ("CFO", 120000, 2),
-       ("CTO", 130000, 3),
+VALUES ("Chief Executive Officer", 150000, 1),
+	("Executive Assistant", 60000, 1),
+       ("Chief Financial Officer", 120000, 2),
        ("Account Analyst", 80000, 2),
-       ("CSO", 130000, 4),
+       ("Chief Technologies Officer", 130000, 3),
+       ("THE IT GUY", 25000, 3),
+       ("Chief Sales Officer", 130000, 4),
+       ("Sales Manager", 65000, 4),
        ("Project Manager", 90000, 5),
        ("Project Engineer", 75000, 5);
 
-INSERT INTO employee (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Clark", "Kent", 1),
-       ("Edwards", "Space", 2),
-       ("Charles", "Bast", 3),
-       ("Eli", "Donalds", 4),
-       ("Gregory", "Mitchell", 5),
-       ("Stewart", "Stalwart", 6);
+       ("Stacy", "Cruz", 2, 1),
+       ("Edwards", "Space", 3),
+       ("Steve", "Cullen", 4, 3)
+       ("Megan", "Bast", 5),
+       ("Eli", "Donalds", 6, 5),
+       ("Gregory", "Mitchell", 7),
+       ("Stewart", "Stalwart", 8, 7),
+       ("John", "Stanza", 9),
+       ("Carol", "Eslz", 10, 9);
+
